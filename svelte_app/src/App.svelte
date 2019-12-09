72683588a8fd9bec87197ebaf24838d647a37136
Jami Kousa <jami.kousa@helsinki.fi>
2019-12-09 21:16:37 +0200

    total crap

--- svelte_app/src/App.svelte
+++ svelte_app/src/App.svelte
@@ -1,38 +1,90 @@
 <script>
   import SvelteInfiniteScroll from "svelte-infinite-scroll";
+  import VirtualList from "@sveltejs/svelte-virtual-list";
 
-  const dimensions = 160;
-	const rows = Math.ceil(window.screen.height / dimensions);
-	const perRow = Math.floor(window.screen.width / dimensions);
+  const dimensions = 80;
+  const rows = Math.ceil(window.screen.height / dimensions);
+  const perRow = Math.floor(window.screen.width / dimensions);
 
-	let items = Math.ceil(rows * perRow);
-	console.log(items)
-  $: items = items;
+  let items = Math.ceil(rows * perRow);
+  let arr = [];
+  for (let index = 0; index < items; index++) {
+    arr.push(index);
+  }
+  const things = [
+    { name: "one", number: 1 },
+    { name: "two", number: 2 },
+    { name: "three", number: 3 },
+        { name: "one", number: 1 },
+    { name: "two", number: 2 },
+    { name: "three", number: 3 },
+        { name: "one", number: 1 },
+    { name: "two", number: 2 },
+    { name: "three", number: 3 },
+        { name: "one", number: 1 },
+    { name: "two", number: 2 },
+    { name: "three", number: 3 },    { name: "one", number: 1 },
+    { name: "two", number: 2 },
+    { name: "three", number: 3 },    { name: "one", number: 1 },
+    { name: "two", number: 2 },
+    { name: "three", number: 3 },    { name: "one", number: 1 },
+    { name: "two", number: 2 },
+    { name: "three", number: 3 },    { name: "one", number: 1 },
+    { name: "two", number: 2 },
+    { name: "three", number: 3 },    { name: "one", number: 1 },
+    { name: "two", number: 2 },
+    { name: "three", number: 3 },    { name: "one", number: 1 },
+    { name: "two", number: 2 },
+    { name: "three", number: 3 },    { name: "one", number: 1 },
+    { name: "two", number: 2 },
+    { name: "three", number: 3 },    { name: "one", number: 1 },
+    { name: "two", number: 2 },
+    { name: "three", number: 3 },    { name: "one", number: 1 },
+    { name: "two", number: 2 },
+    { name: "three", number: 3 },    { name: "one", number: 1 },
+    { name: "two", number: 2 },
+    { name: "three", number: 3 },    { name: "one", number: 1 },
+    { name: "two", number: 2 },
+    { name: "three", number: 3 },
+    { name: "six thousand and ninety-two", number: 6092 }
+  ];
+  /*
+  { length: items, slice: (start, end) => {
+      if (!start || !end) end = items;
+      let arr = [];
+      for (let index = start; index < end; index++) {
+        arr.push(index);
+      }
+      console.log('Defining arr', arr, start, end);
+      return arr;
+    } }
+  */
 </script>
 
 <style>
   div {
     width: 100vw;
     max-height: 100vh;
-		overflow-x: scroll
-	}
-	img {
-		float: left;
-	}
+    overflow-x: scroll;
+  }
+  img {
+    padding: 0 2px;
+  }
 </style>
 
-<div>
+<VirtualList height="500px" items={arr} let:item>
+  <p>{item}</p>
   {#each { length: items } as _, i}
     <img
-			width={dimensions}
-			height={dimensions}
-			style={`min-height: ${dimensions}px; min-width: ${dimensions}px;`}
-      src={`https://picsum.photos/${dimensions / 2}/${dimensions / 2}?cachekill=${Math.random()}`}
+      width={dimensions}
+      height={dimensions}
+      style={`min-height: ${dimensions}px; min-width: ${dimensions}px;`}
+      src={`https://mobvita.cs.helsinki.fi/id/${i}/${dimensions}`}
       alt="whops" />
   {/each}
   <SvelteInfiniteScroll
     threshold={perRow}
     on:loadMore={() => {
-      items = items + (perRow * 10);
+      items = items + perRow * 10;
     }} />
-</div>
+</VirtualList>
