9c82272480beae4b9bdec4fa847bc5a5252e6012
Jami Kousa <jami.kousa@helsinki.fi>
2019-12-10 01:45:37 +0200

    times

--- svelte_app/src/App.svelte
+++ svelte_app/src/App.svelte
@@ -15,10 +15,19 @@
   let above = 0;
   let bottom = window.innerHeight;
 
+  let passedRows = 0;
+  let startTime = new Date().getTime();
+  const interval = setInterval(() => {
+    const timeNow = new Date().getTime();
+    const speed = passedRows / ((timeNow - startTime) / 1000);
+    console.log(speed);
+  }, 1000);
+
   const handleScroll = e => {
     y = e.target.scrollTop;
-    above = Math.floor(y / 84) - 10;
-    bottom = Math.floor((y + window.innerHeight) / 84) + 10
+    if (Math.floor(y / 84) !== above) passedRows++;
+    above = Math.floor(y / 84);
+    bottom = Math.floor((y + window.innerHeight) / 84);
   };
 </script>
 
@@ -35,14 +44,16 @@
 
 <div on:scroll={handleScroll}>
   {#each { length: items } as _, i}
-    {#if ((i + 1) / perRow < above) || ((i + 1) / perRow) > bottom}
-      <img
-        width={dimensions}
-        height={dimensions}
-        style={`min-height: ${dimensions}px; min-width: ${dimensions}px;`}
-        src={`https://avatars2.githubusercontent.com/u/468816?s=400&v=4`}
-        alt="whops" />
+    {#if (i + 1) / perRow < above - 5 || (i + 1) / perRow > bottom + 5}
+      {#if i % 4 == 3}
+        <div
+          style={`min-height: ${dimensions + 4}px; min-width: ${dimensions + 4}px;`} />
+      {/if}
     {:else}
+      {#if i % 4 == 0}
+        <br />
+      {/if}
+
       <img
         width={dimensions}
         height={dimensions}
@@ -50,13 +61,10 @@
         src={`https://mobvita.cs.helsinki.fi/id/${i}/${dimensions}`}
         alt="whops" />
     {/if}
-    {#if i % 4 == 3}
-      <br />
-    {/if}
   {/each}
   <SvelteInfiniteScroll
-    threshold={1000}
+    threshold={80}
     on:loadMore={() => {
-      items = items + perRow * 10;
+      items = items + perRow * 5;
     }} />
 </div>
