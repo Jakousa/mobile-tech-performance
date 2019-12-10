dc7d211f244d53e12732b3e52a64305ee153c5b8
Rochet2 <tqbattlenet@gmail.com>
2019-12-10 16:13:13 +0200

    pwa

--- svelte_app/src/serviceWorker.js
+++ svelte_app/src/serviceWorker.js
@@ -34,7 +34,7 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${HomeURL}/service-worker.js`;
+      const swUrl = `${HomeURL}/serviceWorker.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
