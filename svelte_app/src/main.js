5d994cd4d3ea932635a48de0aaf2ce98444ae4e0
Rochet2 <tqbattlenet@gmail.com>
2019-12-10 16:09:20 +0200

    pwa

--- svelte_app/src/main.js
+++ svelte_app/src/main.js
@@ -1,5 +1,5 @@
 import App from './App.svelte';
-import serviceWorker from '../public/serviceWorker';
+import * as serviceWorker from './serviceWorker';
 
 const app = new App({
 	target: document.body,
